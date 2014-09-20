78f7e4f50397a916297bb433badff0e392e40076
Guillaume Boniface <boniface@boniface-macbookair.roam.corp.google.com>
2014-09-20 22:05:12 +0200

    loadInterventionScript flattened

--- data/apiConnector/rcConnector.js
+++ data/apiConnector/rcConnector.js
@@ -2,55 +2,63 @@ var Crawler = require("crawler").Crawler,
 	path = require('path'),
 	url = require('url'),
 	debug = require('../../debug.js'),
-	connector = {}
-	assert = require('assert');
+	log = require('../../log.js'),
+	connector = {},
+	assert = require('assert'),
+	Q = require('Q');
 
 var lawProjectRegex = /^pjl|^ppl/,
 	extractLawFromUrl = /([^\/]+)\/viz\/interventions.json$/;
 
-connector.loadTree = function(callback){
-	var crawler = new Crawler({
+var crawler = new Crawler({
 		"maxConnections":10
 	}),
 		folderUri = "http://www.lafabriquedelaloi.fr/api/";
 
-	//TO DO refactor to get project list from "dossiers" folder
-	crawler.queue([{
-		"uri": folderUri,
-		"callback": function(err, result, $) {
+connector.loadTree = function(){
+	return Q.promise(function(resolve, reject, notify){
+		//TO DO refactor to get project list from "dossiers" folder
+		//TO DO add a resolve with the full list built
+		crawler.queue([{
+			"uri": folderUri,
+			"callback": function(err, result, $) {
+				if(err){
+					console.log("RC API crawler error: " + err);
+					callback(err);
+				} else {
+					var tree = $("table a").map(function(i, a){
+						return a.innerHTML;
+					}).filter(function(i, a){
+						return lawProjectRegex.exec(a);
+					})
+
+					console.log(tree.length + " law projects");
+
+					tree.each(buildInterventionUrl);
+				}
+			}
+		}]);
+
+		var buildInterventionUrl = function(i, a){
+			var debateUrl = url.resolve(folderUri, path.join(a, 'viz', 'interventions.json'));
+
+			crawler.queue([{
+				"uri": debateUrl,
+				"jQuery":false,
+				"callback": getInterventionJson
+			}]);
+		}
+
+		var getInterventionJson = function(err, result) {
 			if(err){
-				console.log("RC API crawler error: " + err);
-				callback(err);
+				reject(err);
 			} else {
-				$("table a").map(function(i, a){
-					return a.innerHTML;
-				}).filter(function(i, a){
-					return lawProjectRegex.exec(a);
-				}).each(getInterventionJson);
+				debug("Grabbed " + result.body.length + " bytes for " + extractLawFromUrl.exec(result.uri)[1]);
+				notify([extractLawFromUrl.exec(result.uri)[1],
+					new Debates(result.body)]);
 			}
 		}
-	}]);
-
-	var getInterventionJson = function(i, a){
-		var debateUrl = url.resolve(folderUri, path.join(a, 'viz', 'interventions.json'));
-		debug(debateUrl);
-
-		crawler.queue([{
-			"uri": debateUrl,
-			"jQuery":false,
-			"callback": getInterventionText
-		}]);
-	}
-
-	var getInterventionText = function(err, result) {
-		if(err){
-			callback(err);
-		} else {
-			debug("Grabbed " + result.body.length + " bytes");
-			callback(null, [extractLawFromUrl.exec(result.uri)[1],
-				new Debates(result.body)]);
-		}
-	}
+	});
 }
 
 var Group = function(groupSource){
--- data/loadInterventionScript.js
+++ data/loadInterventionScript.js
@@ -1,4 +1,5 @@
-var rcConnector = require('./apiConnector/rcConnector.js'),
+var Q = require('Q'),
+	rcConnector = require('./apiConnector/rcConnector.js'),
 	debug = require('../debug.js'),
 	log = require('../log.js'),
 	Crawler = require('crawler').Crawler,
@@ -8,56 +9,82 @@ var rcConnector = require('./apiConnector/rcConnector.js'),
 	pg = require('./postgresHelper.js'),
 	sourceContract = require('./summaryContract').source;
 
-var extractDivId = /#.+/
+var extractDivId = /#.+/,
+	fetchedUrls;
 
-pg.getTables().then(function(result){
-	var existingSourceTable = false;
-	result.rows.forEach(function(row){
-		existingSourceTable = row.table_name == sourceContract.tableName;
-	});
+var checkExist = function(result){
+	return Q.promise(function(resolve, reject){
+		var existingSourceTable = false;
+		result.rows.forEach(function(row){
+			existingSourceTable = row.table_name == sourceContract.tableName;
+		});
 
-	if( !existingSourceTable ){
-		pg.queryPromise(sourceContract.createDbString())
-			.then(crawlRCApi)
-			.catch(log);
-	} else {
-		crawlRCApi();
-	}
-}).catch(log);
-
-var crawlRCApi = function(){
-	rcConnector.loadTree(function(err, result){
-		var lawId = result[0],
-			debates = result[1],
-			group;
-
-		pg.queryPromise("SELECT source_url FROM source").then(function(fetchedUrls){
-			fetchedUrls = fetchedUrls.rows.map(function(url){ return url["source_url"] });
-
-			debug(lawId);
-			if( debates.getAudience(0) && debates.getAudience(0).getDivision(0).getGroup("Rapporteurs") &&
-				(fetchedUrls.indexOf(debates.getAudience(0).getDivision(0).getGroup("Rapporteurs").getOratorByLengthUri(0))) == -1){
-				crawler.queue([{
-					"uri": debates.getAudience(0).getDivision(0).getGroup("Rapporteurs").getOratorByLengthUri(0),
-					"callback": writeToDb
-				}]);
-			}
-		}).catch(log);
-
-		var writeToDb = function(err, result, $){
-			debug(result.uri);
-			var divId = extractDivId.exec(result.uri)[0];
-			var text = "$$" + $(divId + " .texte_intervention").text() + "$$";
-			var date = debates.getAudience(0).getDivision(0).date;
-			var sqlInsertString = pg.buildSQLInsertString(sourceContract.tableName, sourceContract.getColumns(),
-				["$$" + result.uri + "$$", text, pg.quotify(lawId), pg.quotify(date)]);
-			pg.queryPromise(sqlInsertString)
-				.then(debug)
-				.catch(function(err){
-					console.log(lawId);
-					log(err);
-
-				});
+		if( !existingSourceTable ){
+			pg.queryPromise(sourceContract.createDbString())
+				.then(function(){
+					resolve();
+				})
+				.catch(reject);
+		} else {
+			resolve();
 		}
 	});
 }
+
+var saveFetchedUrls = function(urls){
+	return Q.promise(function(resolve){
+		fetchedUrls = urls.rows.map(function(url){ return url["source_url"] }); 
+		resolve();
+	});
+}
+
+var fetchText = function(result){
+	var lawId = result[0],
+		debates = result[1];
+
+	return Q.promise(function(resolve, reject, notify){
+
+		if( !debates.getAudience(0) || !debates.getAudience(0).getDivision(0).getGroup("Rapporteurs") ){
+			return reject(new Error(lawId + ": debate file incomplete"));
+		}
+		var uri = debates.getAudience(0).getDivision(0).getGroup("Rapporteurs").getOratorByLengthUri(0);
+		if( fetchedUrls.indexOf(uri) != -1 ){
+			return reject();
+		}
+		fetchedUrls.push(uri);
+		crawler.queue([{
+			"uri": uri,
+			"callback": function(err, result, $){
+				if(err){ reject(err); } else { writeToDb([result, $, lawId, debates]) }
+			}
+		}]);
+	});
+};
+
+var writeToDb = function(args){
+	var result = args[0],
+		$ = args[1],
+		lawId = args[2],
+		debates = args[3];
+	debug(result.uri);
+	var divId = extractDivId.exec(result.uri)[0];
+	var text = "$$" + $(divId + " .texte_intervention").text() + "$$";
+	var date = debates.getAudience(0).getDivision(0).date;
+	var sqlInsertString = pg.buildSQLInsertString(sourceContract.tableName, sourceContract.getColumns(),
+		["$$" + result.uri + "$$", text, pg.quotify(lawId), pg.quotify(date)]);
+	pg.queryPromise(sqlInsertString)
+		.then(debug)
+		.catch(log);
+}
+
+
+pg.getTables()
+	.then(checkExist)
+	.then(function(){
+		return pg.queryPromise("SELECT source_url FROM source")
+	})
+	.then(saveFetchedUrls)
+	.then(rcConnector.loadTree)
+	.progress(fetchText)
+	.catch(log)
+	.done();
