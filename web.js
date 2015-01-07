693ed0fd529d3abb36a1a73d75db08950562cf4e
Jean-Baptiste Cid <jbaptiste.cid@gmail.com>
2015-01-07 22:09:59 +0000

    GET /laws/:id working

--- web.js
+++ web.js
@@ -24,6 +24,9 @@ app.get('/laws', function(req, res){
 			"progression" : ["loi-ratification_ordonnance"], //raw_law['status'] TODO fonction status to progression
 			"category" : JSON.parse(raw_law['tags']) // TODO tags avec ids (pour pouvoir filtrer par tags dans l'appli)
 			};
+			if(raw_law['nd_law_title']){
+				law['title'] = raw_law['nd_law_title'];
+			}
 			laws.push(law);
 		});
 
@@ -34,7 +37,21 @@ app.get('/laws', function(req, res){
 app.get('/laws/:id', function(req, res){
 	contract.findLawById(req.params.id)
 	.then(function(results){
-		res.send(results[0]);
+		raw_law = results[0][0];
+		law = {
+		"_id" : raw_law['_id'],
+		"last-update" : raw_law['date'],
+		"title" : raw_law['law_title'],
+		"summary" : raw_law['summary'],
+		"content" : raw_law['content'],
+		"progression" : ["loi-ratification_ordonnance"], //raw_law['status'] TODO fonction status to progression
+		"category" : JSON.parse(raw_law['tags']) // TODO tags avec ids (pour pouvoir filtrer par tags dans l'appli)
+		};
+		if(raw_law['nd_law_title']){
+			law['title'] = raw_law['nd_law_title'];
+		}
+
+		res.json({law: law});
 	}).catch(log);
 });
 
